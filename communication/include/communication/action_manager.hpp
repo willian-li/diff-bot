e109232b734d93005d259c292f2e80925597e791
willian-li <lyzverynice@gmail.com>
2024-07-24 11:33:13 +0800

    test

--- communication/include/communication/action_manager.hpp
+++ communication/include/communication/action_manager.hpp
@@ -4,6 +4,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <memory>
 
 class ActionManager : public rclcpp::Node
@@ -12,13 +13,16 @@ public:
     ActionManager();
     void get_current_position();
     void start_navigation();
-    void move_forward_one_meter();
+    void move_meter(double meter);
+
+    void publish_goal(double x, double y, double z = 0, double qx = 0 , double qy = 0, double qz = 0, double qw = 0);
 
 private:
     void pos_callback(const std_msgs::msg::String::SharedPtr msg) const;
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr pos_subscription_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr goal_publisher_;
 };
 
 #endif  // MY_ROBOT_ACTIONS__ACTION_MANAGER_HPP_
--- communication/src/action_manager.cpp
+++ communication/src/action_manager.cpp
@@ -6,8 +6,34 @@ ActionManager::ActionManager()
 {
     pos_subscription_ = this->create_subscription<std_msgs::msg::String>(
         "/pos", 10, std::bind(&ActionManager::pos_callback, this, std::placeholders::_1));
-    cmd_vel_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    
+    cmd_vel_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
+        "/cmd_vel", 10);
+    goal_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(
+        "/goal_pose", 10);
 }
+
+void ActionManager::publish_goal(double x, double y, double z = 0, double qx = 0 , double qy = 0, double qz = 0, double qw = 0)
+{
+    auto goal_msg = geometry_msgs::msg::PoseStamped();
+    goal_msg.header.stamp = this->get_clock()->now();
+    goal_msg.header.frame_id = "map";
+
+    goal_msg.pose.position.x = x;
+    goal_msg.pose.position.y = y;
+    goal_msg.pose.position.z = z;
+
+    goal_msg.pose.orientation.x = qx;
+    goal_msg.pose.orientation.y = qy;
+    goal_msg.pose.orientation.z = qz;
+    goal_msg.pose.orientation.w = qw;
+
+    RCLCPP_INFO(this->get_logger(), "Publishing goal: [%.2f, %.2f, %.2f] with orientation [%.2f, %.2f, %.2f, %.2f]", 
+                x, y, z, qx, qy, qz, qw);
+
+    goal_publisher_->publish(goal_msg);
+}
+
 
 void ActionManager::get_current_position()
 {
@@ -20,13 +46,22 @@ void ActionManager::start_navigation()
     std::system("ros2 launch my_navigation navigation.launch");
 }
 
-void ActionManager::move_forward_one_meter()
+void ActionManager::move_meter(double meter)
 {
-    RCLCPP_INFO(this->get_logger(), "Moving forward one meter...");
+    RCLCPP_INFO(this->get_logger(), "Moving ...");
     auto twist = geometry_msgs::msg::Twist();
-    twist.linear.x = 1.0;
+    twist.linear.x = meter;
     cmd_vel_publisher_->publish(twist);
     RCLCPP_INFO(this->get_logger(), "Published to /cmd_vel topic to move forward.");
+}
+
+void ActionManager::rotate_yaw(double yaw)
+{
+    RCLCPP_INFO(this->get_logger(), "rotating yaw...");
+    auto twist = geometry_msgs::msg::Twist();
+    twist.angular.z = yaw;
+    cmd_vel_publisher_->publish(twist);
+    RCLCPP_INFO(this->get_logger(), "Published to /cmd_vel topic to rotate.");
 }
 
 void ActionManager::pos_callback(const std_msgs::msg::String::SharedPtr msg) const
